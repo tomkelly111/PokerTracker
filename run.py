b703d99c7da6d8616d687f3b22bd0b2237d6bb4a
tomkelly111 <thomaspkelly111@gmail.com>
2023-01-05 08:26:46 +0000

    add function that validates entry

--- run.py
+++ run.py
@@ -11,10 +11,26 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("pokertracker")
 
 def get_entry_fee():
-    print("Please enter tournament entry fee.")
-    print("Data should be a whole number and not contain commas.")
-    print("Example: 1000")
-    entry_fee = input("Enter your latest tournament entry fee:\n")
+    while True:
+        print("Please enter tournament entry fee.")
+        print("Data should be a whole number and not contain commas.")
+        print("Example: 1000")
+        entry_fee = input("Enter your latest tournament entry fee:\n")
+        if validate_entry(entry_fee):
+            print("Thank you!")
+            break
+        else:
+            continue
     return (entry_fee)
+
+def validate_entry(value):
+    try:
+        int(value)
+        return True
+    except ValueError as e:
+        print(f"Sorry invalid entry:{e}, let's try again! \n")
+        return False
+
+
 
 print(get_entry_fee())
